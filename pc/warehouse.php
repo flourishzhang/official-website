9e130e1134a11e39ff5a2636716b71f1a43c8eab
winston <1522901856@qq.com>
2020-04-26 11:55:36 +0800

    gym更新

--- pc/warehouse.php
+++ pc/warehouse.php
@@ -8,7 +8,7 @@
         <div class="w-wh-title fromTop">
             <div>
                 系统简介
-                <img src="<?php AssetsUrl ();?>/img/warehouse/wh_title.png" alt="">
+                <img src="<?php AssetsUrl ();?>/img/warehouse/wh_title1.png" alt="">
             </div>
         </div>
         <ul class="clearfix">
@@ -44,92 +44,96 @@
             </li>
         </ul>
     </div>
-    <div class="w-wh-box3">
-        <div class="w-wh-title fromTop">
-            <div>
-                当前问题以及解决方法
-                <img src="<?php AssetsUrl ();?>/img/warehouse/wh_title.png" alt="">
+    <div style="background:#f4f8ff;">
+        <div class="w-wh-box3">
+            <div class="w-wh-title fromTop">
+                <div>
+                    当前问题以及解决方法
+                    <img src="<?php AssetsUrl ();?>/img/warehouse/wh_title1.png" alt="">
+                </div>
             </div>
+            <ul>
+                <li class="clearfix">
+                    <div class="w-wh-zi fromRight">
+                        <h3>1.仓库内商品种类繁多,统计难度大</h3>
+                        <p>解决办法:利用自动化仓储管理系统,对入库,出库,等全部进行记 录,让盘点变得十分容易。
+                        </p>
+                    </div>
+                    <div class="w-wh-ge"></div>
+                    <div class="w-wh-tu fromLeft">
+                        <img src="<?php AssetsUrl ();?>/img/warehouse/wh_box3_1.png" alt="">
+                    </div>
+                </li>
+                <li class="clearfix">
+                    <div class="w-wh-tu fromRight">
+                        <img src="<?php AssetsUrl ();?>/img/warehouse/wh_box3_2.png" alt="">
+                    </div>
+                    <div class="w-wh-ge"></div>
+                    <div class="w-wh-zi fromLeft">
+                        <h3>2.仓储内商品借还管理难度大，出错率高,易出现物品丢 失的情况
+                        </h3>
+                        <p>解决办法:利用当前最先进的rfid标签技术,来识别物品进出仓库的 行为。无需人工操作,彻底实现0管理。通过软件,结合录像功能, 保证物品进出仓库事件的可追溯性。
+                        </p>
+                    </div>
+                </li>
+                <li class="clearfix">
+                    <div class="w-wh-zi fromRight">
+                        <h3>3.仓库比较大,即使物品在仓库内,寻找难度也很大</h3>
+                        <p>解决办法:配备rfid手持设备,通过给fid手持设备输入需要寻找的 物品,然后对着仓库的快速大范围的寻找,如果发现对应物品的rfid, 手持设备发出响声。再提高精度缩小寻找范围,直到最终找到物品。
+                        </p>
+                    </div>
+                    <div class="w-wh-ge"></div>
+                    <div class="w-wh-tu fromLeft">
+                        <img src="<?php AssetsUrl ();?>/img/warehouse/wh_box3_3.png" alt="">
+                    </div>
+                </li>
+            </ul>
         </div>
-        <ul>
-            <li class="clearfix">
-                <div class="w-wh-zi fromRight">
-                    <h3>1.仓库内商品种类繁多,统计难度大</h3>
-                    <p>解决办法:利用自动化仓储管理系统,对入库,出库,等全部进行记 录,让盘点变得十分容易。
-                    </p>
-                </div>
-                <div class="w-wh-ge"></div>
-                <div class="w-wh-tu fromLeft">
-                    <img src="<?php AssetsUrl ();?>/img/warehouse/wh_box3_1.png" alt="">
-                </div>
-            </li>
-            <li class="clearfix">
-                <div class="w-wh-tu fromRight">
-                    <img src="<?php AssetsUrl ();?>/img/warehouse/wh_box3_2.png" alt="">
-                </div>
-                <div class="w-wh-ge"></div>
-                <div class="w-wh-zi fromLeft">
-                    <h3>2.仓储内商品借还管理难度大，出错率高,易出现物品丢 失的情况
-                    </h3>
-                    <p>解决办法:利用当前最先进的rfid标签技术,来识别物品进出仓库的 行为。无需人工操作,彻底实现0管理。通过软件,结合录像功能, 保证物品进出仓库事件的可追溯性。
-                    </p>
-                </div>
-            </li>
-            <li class="clearfix">
-                <div class="w-wh-zi fromRight">
-                    <h3>3.仓库比较大,即使物品在仓库内,寻找难度也很大</h3>
-                    <p>解决办法:配备rfid手持设备,通过给fid手持设备输入需要寻找的 物品,然后对着仓库的快速大范围的寻找,如果发现对应物品的rfid, 手持设备发出响声。再提高精度缩小寻找范围,直到最终找到物品。
-                    </p>
-                </div>
-                <div class="w-wh-ge"></div>
-                <div class="w-wh-tu fromLeft">
-                    <img src="<?php AssetsUrl ();?>/img/warehouse/wh_box3_3.png" alt="">
-                </div>
-            </li>
-        </ul>
     </div>
+    
     <div class="w-wh-box4">
         <div class="w-wh-title fromTop">
             <div>
                 功能说明
-                <img src="<?php AssetsUrl ();?>/img/warehouse/wh_title.png" alt="">
+                <img src="<?php AssetsUrl ();?>/img/warehouse/wh_title1.png" alt="">
             </div>
         </div>
         <ul class="clearfix">
-            <li class="fromBottom" style="animation-delay: 0.3s;">
+            <li class="fromBottom" style="animation-delay: 0.3s;margin: 21px 48px 21px 0px;">
+                <img src="<?php AssetsUrl ();?>/img/warehouse/wh_box4_1.png" alt="">
                 <h3>1.物品批量导入功能</h3>
                 <p>
                     当仓库有新的物品需要入仓库时,先将 物品的名称,分组,对应的RFID标签信 息等信息编辑成为-个表格文件,文件 格式可以是xls或是xIsx。然后通过后台 的管理员导入。需要导出时,通过后台 的管理员账号将物品当前信息导出成为 xls或是xIsx ,导出范围可以根据物品分 组,或是进仓库时间等条件。
                 </p>
             </li>
-            <li class="fromBottom" style="animation-delay: 0.5s;margin: 21px 42px;">
+            <li class="fromBottom" style="animation-delay: 0.5s;">
+                <img src="<?php AssetsUrl ();?>/img/warehouse/wh_box4_2.png" alt="">    
                 <h3>2.物品统计功能（盘点功能）</h3>
                 <p>物品统计功能是对当前系统中已有物品 的状态进行统计的功能,包括仓库内物 品数量，种类统计。 统计形式可以是表格,也可以是各种图 表。本功能可以用于辅助库存的人工盘 点，提高工作效率。
                 </p>
             </li>
-            <li class="fromBottom" style="animation-delay: 0.7s;">
+            <li class="fromBottom" style="animation-delay: 0.3s;margin: 21px 48px 21px 0px;">
+                <img src="<?php AssetsUrl ();?>/img/warehouse/wh_box4_3.png" alt="">   
                 <h3>3.物品出入管理功能</h3>
                 <p>在仓库的所有出入口,全部放置一个Rf id标签读卡器,当有Rfid标签从读卡器 的可读取范围内读到标签信号,立刻发 出信息给中央服务器,中央服务器通过 数据库中当前Rfid标签所对应的物品是 在仓库内还是在仓库外来判断该物品本 次触发事件是出仓库还是入仓库,然后 将本次事件记录到数据库中去。
                 </p>
             </li>
-            <li class="fromBottom" style="animation-delay: 0.3s;">
+            <li class="fromBottom" style="animation-delay: 0.5s;">
+                <img src="<?php AssetsUrl ();?>/img/warehouse/wh_box4_4.png" alt="">   
                 <h3>4.RFID管理</h3>
                 <p>本功能是基于RFID可以复用的基础添加 的，RFID本来与仓库中的物品没有直接 关系，物理上我们通过胶布等手段让RF ID绑定到某个物品上,然后在系统中将 那个物品设置为某个RFID。
                 </p>
             </li>
-            <li class="fromBottom" style="animation-delay: 0.5s;margin: 21px 42px;">
+            <li class="fromBottom" style="animation-delay: 0.3s;margin: 21px 48px 21px 0px;">
+                <img src="<?php AssetsUrl ();?>/img/warehouse/wh_box4_5.png" alt="">   
                 <h3>5.管理员管理</h3>
                 <p>当物品导入或是RFID新增时，可以选择 批量导入。如果无法批量导入，需要人 工一个一个的操作，就需要新建几个管 理员，让他们各自分别导入物品或是RF ID,因此本系统多管理员是必须的。
                 </p>
             </li>
-            <li class="fromBottom" style="animation-delay: 0.7s;">
+            <li class="fromBottom" style="animation-delay: 0.5s;">
+                <img src="<?php AssetsUrl ();?>/img/warehouse/wh_box4_6.png" alt="">   
                 <h3>6.权限管理</h3>
                 <p>权限管理不是针对某个单个管理员设置 的,而是增对管理员分组设置的,分组 下的下级管理员分组只能拥有小于或是 登录当前分组的管理权限。
-                </p>
-            </li>
-            <li class="fromBottom" style="animation-delay: 0.3s;">
-                <h3>7.物品快速查找功能</h3>
-                <p>配置手持的RFID读卡设备,首先在RFI D手持终端中输入要寻找的物品，可以 通过搜索功能实现或是通过直接输入的 方式实现。将RFID读卡设备的精度设置 高一点，这样可以判断是否在RFID手持 终端所在前方10米范围内,如果不在, 就去别的位置扫。当手持RFID读卡设备 发出声音,就将精度调低,缩小寻找范 围,就这样一点一点你的缩小识别区域, 最终将物体找到。
                 </p>
             </li>
         </ul>
