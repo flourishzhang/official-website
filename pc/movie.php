39a9249deb743961b5a8ce3349a3687334001b08
winston <1522901856@qq.com>
2020-04-29 11:57:31 +0800

    增加特效

--- pc/movie.php
+++ pc/movie.php
@@ -8,7 +8,7 @@
         <div class="w-mv-title fromTop">
             <div>
                 方案简介
-                <img src="<?php AssetsUrl ();?>/img/movie/moive_title.png" alt="">
+                <img src="<?php AssetsUrl ();?>/img/movie/moive_title1.png" alt="">
             </div>
         </div>
         <div class="clearfix w-mv-div">
@@ -27,132 +27,133 @@
             <div class="w-mv-img fromLeft"><img src="<?php AssetsUrl ();?>/img/movie/moive_box2.png" alt=""></div>
         </div>
     </div>
-    
-    <div class="w-mv-box3">
-        <div class="w-mv-title fromTop">
-            <div>
-                功能简介
-                <img src="<?php AssetsUrl ();?>/img/movie/moive_title.png" alt="">
+    <div style="background: #f4f8ff;">
+        <div class="w-mv-box3">
+            <div class="w-mv-title fromTop">
+                <div>
+                    功能简介
+                    <img src="<?php AssetsUrl ();?>/img/movie/moive_title1.png" alt="">
+                </div>
             </div>
+            <ul class="clearfix">
+                <li class="fromBottom" style="animation-delay: 0.3s;">
+                    <h3>(1)在线购票</h3>
+                    <p>使用场景:消费者在小程序内可直观电影放映场次以及已售出电影票情况,可直接在线选座。目的:减少消费者在柜台排队买票,使购票更快捷方便</p>
+                </li>
+                <li class="fromBottom" style="animation-delay: 0.5s;margin: 21px 42px;">
+                    <h3>(2)商品服务</h3>
+                    <p>使用场景:消费者可以选择购买商品自
+                        取或者外卖送到座位号服务。目的:减
+                        少消费者在柜台排队购买商品以及在观
+                        影途中终止观影购买商品的情况。使消
+                        费者在观影过程中更便捷的购买商品。
+                        </p>
+                </li>
+                <li class="fromBottom" style="animation-delay: 0.7s;">
+                    <h3>(3)会员</h3>
+                    <p>使用场景:消费者在线上申请影院会员,
+                        以及了解会员情况。目的:不用在柜台
+                        办理复杂的会员手续，可自助快速办理
+                        会员，会有有更多的优惠权益。解决了
+                        影城会员招募难的问题。增强用户粘性。
+                        </p>
+                </li>
+                <li class="fromBottom" style="animation-delay: 0.3s;">
+                    <h3>(4)优惠卷</h3>
+                    <p>使用场景:消费者通过参与影院活动获
+                        取优惠券,优惠券可抵扣电影票的金额。
+                        目的:增加与消费者互动,使消费者更
+                        优惠购买电影票,获取消费者好感，增
+                        强消费者粘性。
+                        </p>
+                </li>
+                <li class="fromBottom" style="animation-delay: 0.5s;margin: 21px 42px;">
+                    <h3>(5)社交</h3>
+                    <p>使用场景:消费者可以加入当场观看影
+                        片的群聊,与一起看当场电影的朋友一
+                        起讨论电影。也可以与好友进行聊天，
+                        还可以发布动态目的:增加观众之间的
+                        互动,使此次观影更有趣更有深度。
+                        </p>
+                </li>
+                <li class="fromBottom" style="animation-delay: 0.7s;">
+                    <h3>(6)答题抽奖</h3>
+                    <p>使用场景:消费者通过参与答题可进行
+                        抽奖活动。目的:增加与消费者互动
+                        获取消费者好感,增加消费者的留存率。
+                        代替影院线下依附于工作人员的活动,
+                        提高工作效率。
+                        </p>
+                </li>
+                <li class="fromBottom" style="animation-delay: 0.3s;">
+                    <h3>(7)商城</h3>
+                    <p>使用场景:快速了解电影周边或其他商
+                        品的售卖情况。在线购买电影周边或者
+                        其他商品,直接快递到家。目的:快速
+                        了解电影周边或其他商品的售卖情况。
+                        在家也可以购买电影周边等其他商品。
+                        增加电影周边以及其他商品的销售途径，
+                        提升业绩。
+                        </p>
+                </li>
+                <li class="fromBottom" style="animation-delay: 0.5s;margin: 21px 42px;">
+                    <h3>(8)影评</h3>
+                    <p>使用场景:消费者可对已观影的影片进
+                        行评价。目的:影院能更直接的了解消
+                        费者观影喜好,让电影更有深度。可以
+                        让有观影感受的观众找到共鸣。让消费
+                        者可以表达自己对电影的看法与评价
+                        </p>
+                </li>
+                <li class="fromBottom" style="animation-delay: 0.7s;">
+                    <h3>(9)个人中心</h3>
+                    <p>使用场景:消费者可以直观的查看个人
+                        订单、资金流水、观看记录等。目的:
+                        记录消费者的订单、资金流水、观看记
+                        录等可以让消费者随时了解自己的消费
+                        情况以及观影情况,便于查询。
+                        </p>
+                </li>
+                <li class="fromBottom" style="animation-delay: 0.3s;">
+                    <h3>(10)消息通知</h3>
+                    <p>使用场景:推送系统或者活动消息。目
+                        的:更直观的像消费者推荐影院活动
+                        1
+                        消费者更快速了解影院活动,提高消费
+                        者对于活动的参与率,吸引消费者到影
+                        院观影刺激消费者在商城的消费点。从
+                        而提高影院的业绩。
+                        </p>
+                </li>
+                <li class="fromBottom" style="animation-delay: 0.5s;margin: 21px 42px;">
+                    <h3>(11)呼叫服务</h3>
+                    <p>使用场景:消费者在观影途中可呼叫影
+                        院服务,如更换3D眼睛,影院空气差
+                        1
+                        影片放送有异常,影院有大声喧哗吵闹
+                        等不文明行为。目的:使影院的观影环
+                        境更有秩序更文明,营造舒服的观影环
+                        境。让消费者观影体验更好,提升消费
+                        者好感
+                        </p>
+                </li>
+                <li class="fromBottom" style="animation-delay: 0.7s;">
+                    <h3>(12)用户反馈</h3>
+                    <p>使用场景:消费者可提交对影院的意见。
+                    目的:影院可直接了解消费者的想法和
+                    意见,可更有针对性的提出解决方案,
+                    提升用户体验好感度。
+                    </p>
+                </li>
+            </ul>
         </div>
-        <ul class="clearfix">
-            <li class="fromBottom" style="animation-delay: 0.3s;">
-                <h3>(1)在线购票</h3>
-                <p>使用场景:消费者在小程序内可直观电影放映场次以及已售出电影票情况,可直接在线选座。目的:减少消费者在柜台排队买票,使购票更快捷方便</p>
-            </li>
-            <li class="fromBottom" style="animation-delay: 0.5s;margin: 21px 42px;">
-                <h3>(2)商品服务</h3>
-                <p>使用场景:消费者可以选择购买商品自
-                    取或者外卖送到座位号服务。目的:减
-                    少消费者在柜台排队购买商品以及在观
-                    影途中终止观影购买商品的情况。使消
-                    费者在观影过程中更便捷的购买商品。
-                    </p>
-            </li>
-            <li class="fromBottom" style="animation-delay: 0.7s;">
-                <h3>(3)会员</h3>
-                <p>使用场景:消费者在线上申请影院会员,
-                    以及了解会员情况。目的:不用在柜台
-                    办理复杂的会员手续，可自助快速办理
-                    会员，会有有更多的优惠权益。解决了
-                    影城会员招募难的问题。增强用户粘性。
-                    </p>
-            </li>
-            <li class="fromBottom" style="animation-delay: 0.3s;">
-                <h3>(4)优惠卷</h3>
-                <p>使用场景:消费者通过参与影院活动获
-                    取优惠券,优惠券可抵扣电影票的金额。
-                    目的:增加与消费者互动,使消费者更
-                    优惠购买电影票,获取消费者好感，增
-                    强消费者粘性。
-                    </p>
-            </li>
-            <li class="fromBottom" style="animation-delay: 0.5s;margin: 21px 42px;">
-                <h3>(5)社交</h3>
-                <p>使用场景:消费者可以加入当场观看影
-                    片的群聊,与一起看当场电影的朋友一
-                    起讨论电影。也可以与好友进行聊天，
-                    还可以发布动态目的:增加观众之间的
-                    互动,使此次观影更有趣更有深度。
-                    </p>
-            </li>
-            <li class="fromBottom" style="animation-delay: 0.7s;">
-                <h3>(6)答题抽奖</h3>
-                <p>使用场景:消费者通过参与答题可进行
-                    抽奖活动。目的:增加与消费者互动
-                    获取消费者好感,增加消费者的留存率。
-                    代替影院线下依附于工作人员的活动,
-                    提高工作效率。
-                    </p>
-            </li>
-            <li class="fromBottom" style="animation-delay: 0.3s;">
-                <h3>(7)商城</h3>
-                <p>使用场景:快速了解电影周边或其他商
-                    品的售卖情况。在线购买电影周边或者
-                    其他商品,直接快递到家。目的:快速
-                    了解电影周边或其他商品的售卖情况。
-                    在家也可以购买电影周边等其他商品。
-                    增加电影周边以及其他商品的销售途径，
-                    提升业绩。
-                    </p>
-            </li>
-            <li class="fromBottom" style="animation-delay: 0.5s;margin: 21px 42px;">
-                <h3>(8)影评</h3>
-                <p>使用场景:消费者可对已观影的影片进
-                    行评价。目的:影院能更直接的了解消
-                    费者观影喜好,让电影更有深度。可以
-                    让有观影感受的观众找到共鸣。让消费
-                    者可以表达自己对电影的看法与评价
-                    </p>
-            </li>
-            <li class="fromBottom" style="animation-delay: 0.7s;">
-                <h3>(9)个人中心</h3>
-                <p>使用场景:消费者可以直观的查看个人
-                    订单、资金流水、观看记录等。目的:
-                    记录消费者的订单、资金流水、观看记
-                    录等可以让消费者随时了解自己的消费
-                    情况以及观影情况,便于查询。
-                    </p>
-            </li>
-            <li class="fromBottom" style="animation-delay: 0.3s;">
-                <h3>(10)消息通知</h3>
-                <p>使用场景:推送系统或者活动消息。目
-                    的:更直观的像消费者推荐影院活动
-                    1
-                    消费者更快速了解影院活动,提高消费
-                    者对于活动的参与率,吸引消费者到影
-                    院观影刺激消费者在商城的消费点。从
-                    而提高影院的业绩。
-                    </p>
-            </li>
-            <li class="fromBottom" style="animation-delay: 0.5s;margin: 21px 42px;">
-                <h3>(11)呼叫服务</h3>
-                <p>使用场景:消费者在观影途中可呼叫影
-                    院服务,如更换3D眼睛,影院空气差
-                    1
-                    影片放送有异常,影院有大声喧哗吵闹
-                    等不文明行为。目的:使影院的观影环
-                    境更有秩序更文明,营造舒服的观影环
-                    境。让消费者观影体验更好,提升消费
-                    者好感
-                    </p>
-            </li>
-            <li class="fromBottom" style="animation-delay: 0.7s;">
-                <h3>(12)用户反馈</h3>
-                <p>使用场景:消费者可提交对影院的意见。
-                目的:影院可直接了解消费者的想法和
-                意见,可更有针对性的提出解决方案,
-                提升用户体验好感度。
-                </p>
-            </li>
-        </ul>
     </div>
-
+    
     <div class="w-mv-box4">
         <div class="w-mv-title fromTop">
             <div>
                 方案特点
-                <img src="<?php AssetsUrl ();?>/img/movie/moive_title.png" alt="">
+                <img src="<?php AssetsUrl ();?>/img/movie/moive_title1.png" alt="">
             </div>
         </div>
         <ul class="clearfix">
@@ -198,24 +199,27 @@
             </li>
         </ul>
     </div>
-    <div class="w-mv-box5 clearfix">
-        <div class="w-mv-title fromTop">
-            <div>
-                方案三点
-                <img src="<?php AssetsUrl ();?>/img/movie/moive_title.png" alt="">
+   
+    
+    <div style="background: #f4f8ff;">
+        <div class="w-mv-box5 clearfix">
+            <div class="w-mv-title fromTop">
+                <div>
+                    方案三点
+                    <img src="<?php AssetsUrl ();?>/img/movie/moive_title1.png" alt="">
+                </div>
+            </div>
+            <div class="w-mv-img clearfix">   
+                <div class="fromBottom" style="animation-delay: 0.3s;">
+                    <img src="<?php AssetsUrl ();?>/img/movie/moive_box5_1.png" alt="">
+                </div>
+                <div class="fromBottom" style="animation-delay: 0.5s;margin: 0 36px;">
+                    <img src="<?php AssetsUrl ();?>/img/movie/moive_box5_2.png" alt="">
+                </div>
+                <div class="fromBottom" style="animation-delay: 0.7s;">
+                    <img src="<?php AssetsUrl ();?>/img/movie/moive_box5_3.png" alt="">
+                </div>
             </div>
         </div>
-        <div class="w-mv-img clearfix">   
-            <div class="fromBottom" style="animation-delay: 0.3s;">
-                <img src="<?php AssetsUrl ();?>/img/movie/moive_box5_1.png" alt="">
-            </div>
-            <div class="fromBottom" style="animation-delay: 0.5s;margin: 0 36px;">
-                <img src="<?php AssetsUrl ();?>/img/movie/moive_box5_2.png" alt="">
-            </div>
-            <div class="fromBottom" style="animation-delay: 0.7s;">
-                <img src="<?php AssetsUrl ();?>/img/movie/moive_box5_3.png" alt="">
-            </div>
-        </div>
-        
     </div>
     <?php require ("pc/foot.php");?>
