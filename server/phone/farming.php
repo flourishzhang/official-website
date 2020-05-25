591cae079252c3ca0deef857bfb468889c31b7a8
winston <1522901856@qq.com>
2020-05-25 16:00:09 +0800

    手机页面修改

--- server/phone/farming.php
+++ server/phone/farming.php
@@ -1,65 +1,144 @@
 <?php require ("phone/head.php");?>
-                <div class="w-p-farming-box1">
-                    <img src="<?php echo $webmsg["assetsurl"];?>/img/phone_farming/farming_box1.png" alt="">
+    <div class="w-p-farming-box1">
+        <img src="<?php echo $webmsg["assetsurl"];?>/img/phone_farming/farming_box1.png" alt="">
+    </div>
+    <div class="w-p-farming-box2">
+        <a class="w-ct-return" href="<?php echo $webmsg["siteurl"];?>/mobilesolution.html">返回解决方案列表<span class="iconfont iconxiangzuo"></span></a>
+        <div class="w-wh-title">
+            <div>
+                方案简介
+                <img src="<?php echo $webmsg["assetsurl"];?>/img/phone_farming/title.png" alt="">
+            </div>
+        </div>
+        <img src="<?php echo $webmsg["assetsurl"];?>/img/phone_farming/farming_box2.png" alt="">
+    </div>
+
+    <div style="background:#f4f8ff;">   
+        <div class="w-p-farming-box3">
+            <div class="w-wh-title">
+                <div>
+                    功能介绍
+                    <img src="<?php echo $webmsg["assetsurl"];?>/img/phone_farming/title.png" alt="">
                 </div>
-                <div class="w-p-farming-box2">
-                    <a class="w-ct-return" href="<?php echo $webmsg["siteurl"];?>/mobilesolution.html">返回ALL+列表<span class="iconfont iconxiangzuo"></span></a>
-                    <div class="w-wh-title">
-                        <div>
-                            方案简介
-                            <img src="<?php echo $webmsg["assetsurl"];?>/img/warehouse/wh_title.png" alt="">
-                        </div>
-                    </div>
-                    <img src="<?php echo $webmsg["assetsurl"];?>/img/phone_farming/farming_box2.png" alt="">
+            </div>
+            <div class="clearfix w-tu">
+                <div>
+                    <img src="<?php echo $webmsg["assetsurl"];?>/img/phone_farming/farming_box3_1.png" alt="">
                 </div>
-                <div class="w-p-farming-box3">
-                    <div class="w-wh-title">
-                        <div>
-                            功能介绍
-                            <img src="<?php echo $webmsg["assetsurl"];?>/img/warehouse/wh_title.png" alt="">
-                        </div>
-                    </div>
-                    <div class="clearfix w-tu">
-                        <div>
-                            <img src="<?php echo $webmsg["assetsurl"];?>/img/phone_farming/farming_box3_1.png" alt="">
-                        </div>
-                        <div>
-                            <img src="<?php echo $webmsg["assetsurl"];?>/img/phone_farming/farming_box3_2.png" alt="">
-                        </div>
-                        <div>
-                            <img src="<?php echo $webmsg["assetsurl"];?>/img/phone_farming/farming_box3_3.png" alt="">
-                        </div>
-                        <div>
-                            <img src="<?php echo $webmsg["assetsurl"];?>/img/phone_farming/farming_box3_4.png" alt="">
-                        </div>
-                        <div>
-                            <img src="<?php echo $webmsg["assetsurl"];?>/img/phone_farming/farming_box3_5.png" alt="">
-                        </div>
-                        <div>
-                            <img src="<?php echo $webmsg["assetsurl"];?>/img/phone_farming/farming_box3_6.png" alt="">
-                        </div>
-                    </div>
+                <div>
+                    <img src="<?php echo $webmsg["assetsurl"];?>/img/phone_farming/farming_box3_2.png" alt="">
                 </div>
-                <div class="w-p-farming-box4">
-                    <div class="w-wh-title">
-                        <div>
-                            方案组成
-                            <img src="<?php echo $webmsg["assetsurl"];?>/img/warehouse/wh_title.png" alt="">
-                        </div>
-                    </div>
-                    <div class="clearfix w-tu">
-                        <div>
-                            <img src="<?php echo $webmsg["assetsurl"];?>/img/phone_farming/farming_box4_1.png" alt="">
-                        </div>
-                        <div>
-                            <img src="<?php echo $webmsg["assetsurl"];?>/img/phone_farming/farming_box4_2.png" alt="">
-                        </div>
-                        <div>
-                            <img src="<?php echo $webmsg["assetsurl"];?>/img/phone_farming/farming_box4_3.png" alt="">
-                        </div>
-                        <div>
-                            <img src="<?php echo $webmsg["assetsurl"];?>/img/phone_farming/farming_box4_4.png" alt="">
-                        </div>
-                    </div>
+                <div>
+                    <img src="<?php echo $webmsg["assetsurl"];?>/img/phone_farming/farming_box3_3.png" alt="">
                 </div>
+                <div>
+                    <img src="<?php echo $webmsg["assetsurl"];?>/img/phone_farming/farming_box3_4.png" alt="">
+                </div>
+                <div>
+                    <img src="<?php echo $webmsg["assetsurl"];?>/img/phone_farming/farming_box3_5.png" alt="">
+                </div>
+                <div>
+                    <img src="<?php echo $webmsg["assetsurl"];?>/img/phone_farming/farming_box3_6.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="w-p-farming-box4">
+        <div class="w-wh-title">
+            <div>
+                方案组成
+                <img src="<?php echo $webmsg["assetsurl"];?>/img/phone_farming/title.png" alt="">
+            </div>
+        </div>
+        <div class="clearfix w-tu">
+            <img src="<?php echo $webmsg["assetsurl"];?>/img/phone_farming/farming_box4.png" alt="">
+        </div>
+    </div>
+
+    <div style="background:#f4f8ff;">
+    
+        <div class="w-p-farming-box5">
+            <div class="w-wh-title">
+                <div>
+                    用户app端
+                    <img src="<?php echo $webmsg["assetsurl"];?>/img/phone_farming/title.png" alt="">
+                </div>
+            </div>
+            <p>用户端app以地图的方式对各个控制点进行控制以及查看各个传感器数据。地图可以自定义上传。</p>
+
+            <div class="w-contant clearfix">
+                <div class="w-tu">
+                    <img src="<?php echo $webmsg["assetsurl"];?>/img/phone_farming/farming_box5_1.png" alt="">
+                </div>
+                <div class="w-zi">
+                    <h3>1、查看实时环境数据</h3>
+                    <p>可以实时查看棚内空气、土壤和水中的温度、湿度、二氧化碳浓度、光照、PH值等环境数据。也可以查询历史数据。便于对整体数据分析。</p>
+                    <h3>2、大棚管理</h3>
+                    <p>可新增大棚，也可删除废弃大棚信息。</p>
+                    <h3>3、地图显示</h3>
+                    <p>绿色代表设备正常，红色代表设备异常，灰色代表设备离线。</p>
+                </div>
+            </div>
+
+            <div class="w-contant clearfix">
+                <div class="w-zi">
+                    <h3>1、摄像机显示</h3>
+                    <p>实时获取视频监控，随时查看棚内农作物情况，可以查询历史监控</p>
+                </div>
+                <div class="w-tu">
+                    <img src="<?php echo $webmsg["assetsurl"];?>/img/phone_farming/farming_box5_2.png" alt="">
+                </div>
+            </div>
+
+            <div class="w-contant clearfix">
+                <div class="w-tu">
+                    <img src="<?php echo $webmsg["assetsurl"];?>/img/phone_farming/farming_box5_3.png" alt="">
+                </div>
+                <div class="w-zi">
+                    <h3>1、记录显示</h3>
+                    <p>显示记录采用轮播的方式，旧记录会显示在下方，新记录会显示在上方</p>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="w-p-farming-box6">
+        <div class="w-wh-title">
+            <div>
+                后台
+                <img src="<?php echo $webmsg["assetsurl"];?>/img/phone_farming/title.png" alt="">
+            </div>
+        </div>
+
+        <div class="w-contant clearfix">
+            <div class="w-zi">
+                <h3>1、权限管理.</h3>
+                <p>权限管理分为管理员管理、角色管理、管理员操作日志。可对管理员的权限进行编辑。</p>
+                <h3>2、App用户管理</h3>
+                <p>权限管理分为用户管理、用户组管理。可对用户进行分组以及权限管理。</p>
+            </div>
+            <div class="w-tu">
+                <img src="<?php echo $webmsg["assetsurl"];?>/img/phone_farming/farming_box6_1.png" alt="">
+            </div>
+        </div>
+
+        <div class="w-contant clearfix">
+            <div class="w-tu">
+                <img src="<?php echo $webmsg["assetsurl"];?>/img/phone_farming/farming_box6_2.png" alt="">
+            </div>
+            <div class="w-zi">
+                <h3>1、规则管理</h3>
+                <p>可对控制器规则进行管理。</p>
+                <h3>2、数据管理</h3>
+                <p>管理ph值、光照、温度、湿度、二氧化碳进行数据分析等等功能</p>
+                <h3>3、控制器管理.</h3>
+                <p>编辑可控制内容，设定控制规则等等对控制器进行管理</p>
+            </div>
+        </div>
+
+    </div>
+
 <?php require ("phone/foot.php");?>
