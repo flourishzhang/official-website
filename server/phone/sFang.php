46a98c690977719f0fbc26d8954eb059b3046832
Shenyinyin <you@example.com>
2020-05-26 17:53:37 +0800

    修改手机版解决方案

--- server/phone/sFang.php
+++ server/phone/sFang.php
@@ -13,44 +13,60 @@
             <ul  class="clearfix">
                 <li>
                     <a href="<?php echo $webmsg["siteurl"];?>/mobilegym.html">
-                        <img src="<?php echo $webmsg["assetsurl"];?>/img/s-fang/f1.png">
+                        <img src="<?php echo $webmsg["assetsurl"];?>/img/s-fang/fang-index1.png">
+                        <h4>仓储管理系统</h4>
+                        <p>
+                        系统采用业界主流的硬件平台、操作系统平台、数据库平台以及标准的协议，
+                        保证系统的开放性。采用分散控制、集中管理的结构，使得系统可扩性很强
+                        </p>
                     </a>
                 </li>
                 <li>
                     <a href="<?php echo $webmsg["siteurl"];?>/mobilecommunity.html">
-                        <img src="<?php echo $webmsg["assetsurl"];?>/img/s-fang/f2.png">
+                        <img src="<?php echo $webmsg["assetsurl"];?>/img/s-fang/fang-index2.png">
+                        <h4>智慧小区</h4>
+                        <p>
+                        利用传感网、互联网、移动互联网及4G网络和相关终端，将建筑小区以物业管理系统为基础，把小区内各类公共信息应用系统和业主家居应用系统等系统进行集成。
+                        </p>
                     </a>
                 </li>
                 <li>
                     <a href="<?php echo $webmsg["siteurl"];?>/mobilepowerful.html">
-                        <img src="<?php echo $webmsg["assetsurl"];?>/img/s-fang/f3.png">
+                        <img src="<?php echo $webmsg["assetsurl"];?>/img/s-fang/fang-index3.png">
+                        <h4>电力远程控制</h4>
+                        <p>
+                        通过网络监控各个设备结点，以分散监控集中管理的方式有效解决用电单位电气线路老旧、小微企业无专业电工、肉眼无法直观系统即时排查电气隐患、隐蔽工程隐患检查难等难题。
+                        </p>
                     </a>
                 </li>
                 <li>
                     <a href="<?php echo $webmsg["siteurl"];?>/mobileagriculture.html">
-                        <img src="<?php echo $webmsg["assetsurl"];?>/img/s-fang/f4.png">
+                        <img src="<?php echo $webmsg["assetsurl"];?>/img/s-fang/fang-index4.png">
+                        <h4>智慧农业</h4>
+                        <p>
+                        主要由农业温室大棚、各种无线传感器、控制器及后台系统软件和用户端AP等组成。
+                        </p>
                     </a>
                 </li>
                 <li>
                     <a href="<?php echo $webmsg["siteurl"];?>/mobilecinema.html">
-                        <img src="<?php echo $webmsg["assetsurl"];?>/img/s-fang/f5.png">
+                        <img src="<?php echo $webmsg["assetsurl"];?>/img/s-fang/fang-index5.png">
+                        <h4>影院服务平台</h4>
+                        <p>
+                        影院服务平台方案包含线上用户端小程序与影院后台两个部分，线下无纸化检票系统。
+                        </p>
                     </a>
                 </li>
                 <li>
                     <a href="<?php echo $webmsg["siteurl"];?>/mobilecybercafe.html">
-                        <img src="<?php echo $webmsg["assetsurl"];?>/img/s-fang/f6.png">
+                        <img src="<?php echo $webmsg["assetsurl"];?>/img/s-fang/fang-index6.png">
+                        <h4>仓储管理系统</h4>
+                        <p>
+                        利用app开门,支付宝/微信收费的方式实现无人化，24小时营业的无人网吧。
+                        </p>
                     </a>
                 </li>
-                <!-- <li>
-                    <a href="<?php echo $webmsg["siteurl"];?>/phonesFang.html">
-                        <img src="<?php echo $webmsg["assetsurl"];?>/img/s-fang/f7.png">
-                    </a>
-                </li>
-                <li>
-                    <a href="<?php echo $webmsg["siteurl"];?>/phonesFang.html">
-                        <img src="<?php echo $webmsg["assetsurl"];?>/img/s-fang/f8.png">
-                    </a>
-                </li> -->
+
             </ul>
         </div>
     </div>
