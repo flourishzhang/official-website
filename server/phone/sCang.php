d599bbb75807de962e401fc689733bf4fc32d285
Shenyinyin <you@example.com>
2020-05-28 15:15:43 +0800

    添加仓库

--- server/phone/sCang.php
+++ server/phone/sCang.php
@@ -16,22 +16,22 @@
         <div class="y-ph-cang-body2">
             <ul class="clearfix">
                 <li>
-                    <img src="<?php echo $webmsg["assetsurl"];?>/img/s-cang/x1.png" alt="">
+                    <img src="<?php echo $webmsg["assetsurl"];?>/img/s-cang/back1.png" alt="">
                 </li>
-                <li >
-                    <img src="<?php echo $webmsg["assetsurl"];?>/img/s-cang/x2.png" alt="">
+                <li>
+                    <img src="<?php echo $webmsg["assetsurl"];?>/img/s-cang/back2.png" alt="">
                 </li>
-                <li >
-                    <img src="<?php echo $webmsg["assetsurl"];?>/img/s-cang/x3.png" alt="">
+                <li>
+                    <img src="<?php echo $webmsg["assetsurl"];?>/img/s-cang/back3.png" alt="">
                 </li>
-                <li >
-                    <img src="<?php echo $webmsg["assetsurl"];?>/img/s-cang/x4.png" alt="">
+                <li>
+                    <img src="<?php echo $webmsg["assetsurl"];?>/img/s-cang/back4.png" alt="">
                 </li>
-                <li >
-                    <img src="<?php echo $webmsg["assetsurl"];?>/img/s-cang/x5.png" alt="">
+                <li>
+                    <img src="<?php echo $webmsg["assetsurl"];?>/img/s-cang/back5.png" alt="">
                 </li>
-                <li >
-                    <img src="<?php echo $webmsg["assetsurl"];?>/img/s-cang/x6.png" alt="">
+                <li>
+                    <img src="<?php echo $webmsg["assetsurl"];?>/img/s-cang/back6.png" alt="">
                 </li>
             </ul>
         </div>
@@ -43,48 +43,63 @@
             </div>
         </div>
         <div class="y-ph-cang-body3  ">
-            <ul >
-                <li>
-                    <img src="<?php echo $webmsg["assetsurl"];?>/img/s-cang/c1.png" alt="">
-                </li>
-                <li>
-                    <img src="<?php echo $webmsg["assetsurl"];?>/img/s-cang/c2.png" alt="">
-                </li>
-                <li>
-                    <img src="<?php echo $webmsg["assetsurl"];?>/img/s-cang/c3.png" alt="">
-                </li>
+            <ul class="clearfix">
+               <li>
+                    <img src="<?php echo $webmsg["assetsurl"];?>/img/s-cang/cang-j1.png" alt="">
+                    <h4>1.仓库内商品种类繁多,统计难度大</h4>
+                    <p>解决办法:利用自动化仓储管理系统,对入库,出库,等全部进行记 录,让盘点变得十分容易。 </p>
+               </li>
+               <li>
+                    <img src="<?php echo $webmsg["assetsurl"];?>/img/s-cang/cang-j2.png" alt="">
+                    <h4>2.仓储内商品借还管理难度大，出错率高,易出现物品丢 失的情况</h4>
+                    <p>解决办法:利用当前最先进的rfid标签技术,来识别物品进出仓库的行为。</p>
+               </li>
+               <li>
+                    <img src="<?php echo $webmsg["assetsurl"];?>/img/s-cang/cang-j3.png" alt="">
+                    <h4>3.仓库比较大,即使物品在仓库内,寻找难度也很大</h4>
+                    <p>能通过rifd手持设备输入商品，在仓库快速寻找商品。发现商品后设备发出响声，提高精确度缩小范围找到物品。</p>
+               </li>
             </ul>
         </div>
     </div>
     <div class="y-ph-cang-box4">
         <div class="y-ph-cang-title4">
-            <div class="title3">功能说明
+            <div class="title4">功能说明
                 <img src="<?php echo $webmsg["assetsurl"];?>/img/s-cang/t1.png" alt="">
             </div>
         </div>
         <div class="y-ph-cang-body4  ">
             <ul class="clearfix">
                 <li>
-                    <img src="<?php echo $webmsg["assetsurl"];?>/img/s-cang/g1.png" alt="">
-                </li>
+                    <img src="<?php echo $webmsg["assetsurl"];?>/img/s-cang/cang-g1.png" alt="">
+                    <h4>1.物品批量导入功能</h4>
+                    <!-- <p> 当仓库有新的物品需要入仓库时,先将物品的名称,分组,对应的RFID标签信息等信息编辑成为-个表格文件,文件 格式可以是xls或是xIsx。然后通过后台 的管理员导入。需要导出时,通过后台的管理员账号将物品当前信息导出成为xls或是xIsx ,导出范围可以根据物品分组,或是进仓库时间等条件。</p> -->
+               </li>
                 <li>
-                    <img src="<?php echo $webmsg["assetsurl"];?>/img/s-cang/g2.png" alt="">
-                </li>
+                    <img src="<?php echo $webmsg["assetsurl"];?>/img/s-cang/cang-g2.png" alt="">
+                    <h4>2.物品统计功能（盘点功能）</h4>
+                    <!-- <p>物品统计功能是对当前系统中已有物品的状态进行统计的功能,包括仓库内物品数量，种类统计。 统计形式可以是表格,也可以是各种图表。本功能可以用于辅助库存的人工盘点，提高工作效率。</p> -->
+               </li>
                 <li>
-                    <img src="<?php echo $webmsg["assetsurl"];?>/img/s-cang/g3.png" alt="">
-                </li>
+                    <img src="<?php echo $webmsg["assetsurl"];?>/img/s-cang/cang-g3.png" alt="">
+                    <h4>3.物品出入管理功能</h4>
+                    <!-- <p>在仓库的所有出入口,全部放置一个Rfid标签读卡器,当有Rfid标签从读卡器的可读取范围内读到标签信号,立刻发出信息给中央服务器,中央服务器通过数据库中当前Rfid标签所对应的物品是在仓库内还是在仓库外来判断该物品本次触发事件是出仓库还是仓库,然后 将本次事件记录到数据库中去。</p> -->
+               </li>
                 <li>
-                    <img src="<?php echo $webmsg["assetsurl"];?>/img/s-cang/g4.png" alt="">
-                </li>
+                    <img src="<?php echo $webmsg["assetsurl"];?>/img/s-cang/cang-g4.png" alt="">
+                    <h4>4.RFID管理</h4>
+                    <!-- <p>本功能是基于RFID可以复用的基础添加 的，RFID本来与仓库中的物品没有直接 关系，物理上我们通过胶布等手段让RFID绑定到某个物品上,然后在系统中将那个物品设置为某个RFID。</p> -->
+               </li>
                 <li>
-                    <img src="<?php echo $webmsg["assetsurl"];?>/img/s-cang/g5.png" alt="">
-                </li>
+                    <img src="<?php echo $webmsg["assetsurl"];?>/img/s-cang/cang-g5.png" alt="">
+                    <h4>5.管理员管理</h4>
+                    <!-- <p>当物品导入或是RFID新增时，可以选择批量导入。如果无法批量导入，需要人工一个一个的操作，就需要新建几个管理员，让他们各自分别导入物品或是RFID,因此本系统多管理员是必须的。</p> -->
+               </li>
                 <li>
-                    <img src="<?php echo $webmsg["assetsurl"];?>/img/s-cang/g6.png" alt="">
-                </li>
-                <li>
-                    <img src="<?php echo $webmsg["assetsurl"];?>/img/s-cang/g7.png" alt="">
-                </li>
+                    <img src="<?php echo $webmsg["assetsurl"];?>/img/s-cang/cang-g6.png" alt="">
+                    <h4>6.权限管理</h4>
+                    <!-- <p>权限管理不是针对某个单个管理员设置的,而是增对管理员分组设置的,分组下的下级管理员分组只能拥有小于或是登录当前分组的管理权限。</p> -->
+               </li>
             </ul>
         </div>
     </div>
