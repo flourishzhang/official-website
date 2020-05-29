b420afef07b7d28fa404160840a224d72b40dd78
Shenyinyin <you@example.com>
2020-05-29 16:04:58 +0800

    修改bug4

--- server/pc/fang.php
+++ server/pc/fang.php
@@ -28,7 +28,7 @@
                     <a href="<?php echo $webmsg["siteurl"];?>/community.html">
                     <img src="<?php echo $webmsg["assetsurl"];?>/img/fang/f2.png">
                     <h3> 智慧小区</h3>
-                    <p class="y-text1">系统采用业界主流的硬件平台、操作系统平台、数据库平台以及标准的协议， 保证系统的开放性。采用分散控制、集中管理的结构，使得系统可扩性很强。
+                    <p class="y-text1"> 利用传感网、互联网、移动互联网及4G网络和相关终端，将建筑小区以物业管理系统为基础，把小区内各类公共信息应用系统和业主家居应用系统等系统进行集成。
                     </p>
                     <p class="y-more1">查看更多
                         <span class="iconfont iconxiangyou"></span>
@@ -39,7 +39,7 @@
                     <a href="<?php echo $webmsg["siteurl"];?>/powerful.html">
                     <img src="<?php echo $webmsg["assetsurl"];?>/img/fang/f3.png">
                     <h3> 电力远程控制</h3>
-                    <p class="y-text1">系统采用业界主流的硬件平台、操作系统平台、数据库平台以及标准的协议， 保证系统的开放性。采用分散控制、集中管理的结构，使得系统可扩性很强。
+                    <p class="y-text1"> 通过网络监控各个设备结点，以分散监控集中管理的方式有效解决用电单位电气线路老旧、小微企业无专业电工、肉眼无法直观系统即时排查电气隐患、隐蔽工程隐患检查难等难题。
                     </p>
                     <p class="y-more1">查看更多
                         <span class="iconfont iconxiangyou"></span>
@@ -50,7 +50,7 @@
                     <a href="<?php echo $webmsg["siteurl"];?>/agriculture.html">
                     <img src="<?php echo $webmsg["assetsurl"];?>/img/fang/f4.png">
                     <h3> 智慧农业</h3>
-                    <p class="y-text1">系统采用业界主流的硬件平台、操作系统平台、数据库平台以及标准的协议， 保证系统的开放性。采用分散控制、集中管理的结构，使得系统可扩性很强。
+                    <p class="y-text1"> 主要由农业温室大棚、各种无线传感器、控制器及后台系统软件和用户端AP等组成。
                     </p>
                     <p class="y-more1">查看更多
                         <span class="iconfont iconxiangyou"></span>
@@ -61,7 +61,7 @@
                     <a href="<?php echo $webmsg["siteurl"];?>/cinema.html">
                     <img src="<?php echo $webmsg["assetsurl"];?>/img/fang/f5.png">
                     <h3> 影院服务平台</h3>
-                    <p class="y-text1">系统采用业界主流的硬件平台、操作系统平台、数据库平台以及标准的协议， 保证系统的开放性。采用分散控制、集中管理的结构，使得系统可扩性很强。
+                    <p class="y-text1"> 影院服务平台方案包含线上用户端小程序与影院后台两个部分，线下无纸化检票系统。
                     </p>
                     <p class="y-more1">查看更多
                         <span class="iconfont iconxiangyou"></span>
@@ -72,7 +72,7 @@
                     <a href="<?php echo $webmsg["siteurl"];?>/cybercafe.html">
                     <img src="<?php echo $webmsg["assetsurl"];?>/img/fang/f6.png">
                     <h3> 无人网吧</h3>
-                    <p class="y-text1">系统采用业界主流的硬件平台、操作系统平台、数据库平台以及标准的协议， 保证系统的开放性。采用分散控制、集中管理的结构，使得系统可扩性很强。
+                    <p class="y-text1"> 利用app开门,支付宝/微信收费的方式实现无人化，24小时营业的无人网吧。
                     </p>
                     <p class="y-more1">查看更多
                         <span class="iconfont iconxiangyou"></span>
