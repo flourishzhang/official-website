c649853b389514af8d1ed11fb8cc8b3cad6256f7
jevian ma（马作伟） <jevian_ma@worldflying.cn>
2021-01-12 22:23:39 +0800

    修改sitemap.php，让其与主站无关

--- server/pc/sitemap.php
+++ server/pc/sitemap.php
@@ -3,32 +3,32 @@ $newsList = GetNewsList ("`articleid`", 1, null, null, 1, null, null, true);
 $len = count($newsList);
 for ($i = 0 ; $i < $len ; $i++) {
 ?>
-https://www.worldflying.cn/article-id-<?php echo $newsList[$i]["articleid"];?>.html
-https://www.worldflying.cn/mobilearticle-id-<?php echo $newsList[$i]["articleid"];?>.html
+<?php echo $webmsg["siteurl"];?>/article-id-<?php echo $newsList[$i]["articleid"];?>.html
+<?php echo $webmsg["siteurl"];?>/mobilearticle-id-<?php echo $newsList[$i]["articleid"];?>.html
 <?php
 }
 ?>
-https://www.worldflying.cn/index.html
-https://www.worldflying.cn/iot.html
-https://www.worldflying.cn/solution.html
-https://www.worldflying.cn/news.html
-https://www.worldflying.cn/about.html
-https://www.worldflying.cn/recruit.html
-https://www.worldflying.cn/gym.html
-https://www.worldflying.cn/agriculture.html
-https://www.worldflying.cn/powerful.html
-https://www.worldflying.cn/cinema.html
-https://www.worldflying.cn/cybercafe.html
-https://www.worldflying.cn/community.html
-https://www.worldflying.cn/mobileindex.html
-https://www.worldflying.cn/mobileiot.html
-https://www.worldflying.cn/mobilesolution.html
-https://www.worldflying.cn/mobilenews.html
-https://www.worldflying.cn/mobileabout.html
-https://www.worldflying.cn/mobilerecruit.html
-https://www.worldflying.cn/mobilegym.html
-https://www.worldflying.cn/mobileagriculture.html
-https://www.worldflying.cn/mobilepowerful.html
-https://www.worldflying.cn/mobilecinema.html
-https://www.worldflying.cn/mobilecybercafe.html
-https://www.worldflying.cn/mobilecommunity.html
+<?php echo $webmsg["siteurl"];?>/index.html
+<?php echo $webmsg["siteurl"];?>/iot.html
+<?php echo $webmsg["siteurl"];?>/solution.html
+<?php echo $webmsg["siteurl"];?>/news.html
+<?php echo $webmsg["siteurl"];?>/about.html
+<?php echo $webmsg["siteurl"];?>/recruit.html
+<?php echo $webmsg["siteurl"];?>/gym.html
+<?php echo $webmsg["siteurl"];?>/agriculture.html
+<?php echo $webmsg["siteurl"];?>/powerful.html
+<?php echo $webmsg["siteurl"];?>/cinema.html
+<?php echo $webmsg["siteurl"];?>/cybercafe.html
+<?php echo $webmsg["siteurl"];?>/community.html
+<?php echo $webmsg["siteurl"];?>/mobileindex.html
+<?php echo $webmsg["siteurl"];?>/mobileiot.html
+<?php echo $webmsg["siteurl"];?>/mobilesolution.html
+<?php echo $webmsg["siteurl"];?>/mobilenews.html
+<?php echo $webmsg["siteurl"];?>/mobileabout.html
+<?php echo $webmsg["siteurl"];?>/mobilerecruit.html
+<?php echo $webmsg["siteurl"];?>/mobilegym.html
+<?php echo $webmsg["siteurl"];?>/mobileagriculture.html
+<?php echo $webmsg["siteurl"];?>/mobilepowerful.html
+<?php echo $webmsg["siteurl"];?>/mobilecinema.html
+<?php echo $webmsg["siteurl"];?>/mobilecybercafe.html
+<?php echo $webmsg["siteurl"];?>/mobilecommunity.html
